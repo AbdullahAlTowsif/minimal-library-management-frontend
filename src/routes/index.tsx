eb3be260d7ac6d457e205bd8263f43c4fcb8f36d
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-07-02 03:27:40 +0600

    create book form desin added

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import MainLayout from "@/layouts/MainLayout";
 import AllBooks from "@/pages/AllBooks";
+import CreateBook from "@/pages/CreateBook";
 import { createBrowserRouter } from "react-router";
 
 export const router = createBrowserRouter([
@@ -10,5 +11,9 @@ export const router = createBrowserRouter([
   {
     path: "/books",
     Component: AllBooks,
+  },
+  {
+    path: "/create-book",
+    Component: CreateBook,
   }
 ]);
