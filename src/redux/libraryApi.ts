180ac2a33a7f5b1c2716e3c957976486b2969412
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-07-03 02:33:55 +0600

    backend deployed baseUrl added and redeployed

--- src/redux/libraryApi.ts
+++ src/redux/libraryApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const libraryApi = createApi({
   reducerPath: "libraryApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://minimal-library-management-backend.vercel.app/api" }),
   tagTypes: ["books"],
   endpoints: (builder) => ({
     getBooks: builder.query({
